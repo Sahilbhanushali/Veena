93c73d7b4b99c7c20ca079de507beb9a91dff7b9
Sahil Bhanushali <sahilbhanushali5678@gmail.com>
2025-09-23 15:49:28 +0530

    removeZipfiles

--- mailer/forms_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/* Forms settings */
-	$subj = "New message from 'Veena Website'"; //letter subject
-	$to = 'info@niilkanth.com'; // Enter Your E-mail
-	$from = 'veena@niilkanth.com'; // Admin e-mail
-	$fromName = 'NiilkanthVeena'; // Your company name
-	$charset = 'UTF-8';
-?>
